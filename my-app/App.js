4e5c5b1faa87b37d0983a8086d3731ea05c9a2ad
GarretRen <923019070@qq.com>
2020-09-28 18:52:26 +0800

    Pedometer
    Use lagacy version for Pedometer. Need testing.

--- my-app/App.js
+++ my-app/App.js
@@ -1,39 +1,82 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
-import * as Location from "expo-location";
+import React from "react";
+import { Pedometer } from "expo-legacy";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+export default class App extends React.Component {
+  state = {
+    isPedometerAvailable: "checking",
+    pastStepCount: 0,
+    currentStepCount: 0,
+  };
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+  componentDidMount() {
+    this._subscribe();
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-    </View>
-  );
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _subscribe = () => {
+    this._subscription = Pedometer.watchStepCount((result) => {
+      this.setState({
+        currentStepCount: result.steps,
+      });
+    });
+
+    Pedometer.isAvailableAsync().then(
+      (result) => {
+        this.setState({
+          isPedometerAvailable: String(result),
+        });
+      },
+      (error) => {
+        this.setState({
+          isPedometerAvailable: "Could not get isPedometerAvailable: " + error,
+        });
+      }
+    );
+
+    const end = new Date();
+    const start = new Date();
+    start.setDate(end.getDate() - 1);
+    Pedometer.getStepCountAsync(start, end).then(
+      (result) => {
+        this.setState({ pastStepCount: result.steps });
+      },
+      (error) => {
+        this.setState({
+          pastStepCount: "Could not get stepCount: " + error,
+        });
+      }
+    );
+  };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}
+        </Text>
+        <Text>
+          Steps taken in the last 24 hours: {this.state.pastStepCount}
+        </Text>
+        <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 15,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
