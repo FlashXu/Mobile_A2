4351328cec6887ee67570875212d5d83af61a898
YiziH <69949508+YiziH@users.noreply.github.com>
2020-09-29 15:36:39 +1000

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,60 +1,45 @@
 import React from "react";
 import { Pedometer } from "expo-legacy";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+
 
 export default class App extends React.Component {
   state = {
     isPedometerAvailable: "checking",
     pastStepCount: 0,
-    currentStepCount: 0,
+    currentStepCount: 0
   };
-
-  componentDidMount() {
-    this._subscribe();
-  }
 
   componentWillUnmount() {
-    this._unsubscribe();
+    this._subscription && this._subscription.remove();
+    delete this._subscription;
   }
 
-  _subscribe = () => {
-    this._subscription = Pedometer.watchStepCount((result) => {
-      this.setState({
-        currentStepCount: result.steps,
+  init = async () => {
+    let isPedometerAvailable = false;
+    try {
+      isPedometerAvailable = await Pedometer.isAvailableAsync();
+      this.setState({ isPedometerAvailable: String(isPedometerAvailable) });
+    } catch ({ message }) {
+      this.setState({ isPedometerAvailable: `Could not get isPedometerAvailable: ${message}` });
+    }
+    if (isPedometerAvailable) {
+      this._subscription = Pedometer.watchStepCount(result => {
+        this.setState({
+          currentStepCount: result.steps
+        });
       });
-    });
-
-    Pedometer.isAvailableAsync().then(
-      (result) => {
-        this.setState({
-          isPedometerAvailable: String(result),
-        });
-      },
-      (error) => {
-        this.setState({
-          isPedometerAvailable: "Could not get isPedometerAvailable: " + error,
-        });
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - 1);
+      try {
+        const { steps } = await Pedometer.getStepCountAsync(start, end);
+        this.setState({ pastStepCount: steps });
+      } catch ({ message }) {
+        this.setState({ pastStepCount: `Could not get stepCount: ${message}` });
       }
-    );
-
-    const end = new Date();
-    const start = new Date();
-    start.setDate(end.getDate() - 1);
-    Pedometer.getStepCountAsync(start, end).then(
-      (result) => {
-        this.setState({ pastStepCount: result.steps });
-      },
-      (error) => {
-        this.setState({
-          pastStepCount: "Could not get stepCount: " + error,
-        });
-      }
-    );
-  };
-
-  _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
+    }
   };
 
   render() {
@@ -67,6 +52,7 @@ export default class App extends React.Component {
           Steps taken in the last 24 hours: {this.state.pastStepCount}
         </Text>
         <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
+        <Button title="INIT" onPress={this.init} />
       </View>
     );
   }
@@ -77,6 +63,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 15,
     alignItems: "center",
-    justifyContent: "center",
-  },
+    justifyContent: "center"
+  }
 });
