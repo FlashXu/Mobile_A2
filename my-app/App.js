e358e31770427b77bdc5ca212082d2d2d3cfe3b1
GarretRen <923019070@qq.com>
2020-10-02 17:13:52 +0800

    backend-locaiton-tracking
    Need testing.

--- my-app/App.js
+++ my-app/App.js
@@ -1,59 +1,25 @@
 import React from "react";
-import { Pedometer } from "expo-legacy";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { Text, TouchableOpacity, StyleSheet, View } from "react-native";
+import * as TaskManager from "expo-task-manager";
+import * as Location from "expo-location";
 
+const LOCATION_TASK_NAME = "background-location-task";
 
-
-export default class App extends React.Component {
-  state = {
-    isPedometerAvailable: "checking",
-    pastStepCount: 0,
-    currentStepCount: 0
-  };
-
-  componentWillUnmount() {
-    this._subscription && this._subscription.remove();
-    delete this._subscription;
-  }
-
-  init = async () => {
-    let isPedometerAvailable = false;
-    try {
-      isPedometerAvailable = await Pedometer.isAvailableAsync();
-      this.setState({ isPedometerAvailable: String(isPedometerAvailable) });
-    } catch ({ message }) {
-      this.setState({ isPedometerAvailable: `Could not get isPedometerAvailable: ${message}` });
-    }
-    if (isPedometerAvailable) {
-      this._subscription = Pedometer.watchStepCount(result => {
-        this.setState({
-          currentStepCount: result.steps
-        });
+export default class Component extends React.Component {
+  onPress = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+    if (status === "granted") {
+      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        accuracy: Location.Accuracy.Balanced,
       });
-      const end = new Date();
-      const start = new Date();
-      start.setDate(end.getDate() - 1);
-      try {
-        const { steps } = await Pedometer.getStepCountAsync(start, end);
-        this.setState({ pastStepCount: steps });
-      } catch ({ message }) {
-        this.setState({ pastStepCount: `Could not get stepCount: ${message}` });
-      }
     }
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          Pedometer.isAvailableAsync(): {this.state.isPedometerAvailable}
-        </Text>
-        <Text>
-          Steps taken in the last 24 hours: {this.state.pastStepCount}
-        </Text>
-        <Text>Walk! And watch this go up: {this.state.currentStepCount}</Text>
-        <Button title="INIT" onPress={this.init} />
-      </View>
+      <TouchableOpacity onPress={this.onPress}>
+        <Text>Enable background location</Text>
+      </TouchableOpacity>
     );
   }
 }
@@ -61,8 +27,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 15,
-    alignItems: "center",
-    justifyContent: "center"
+  },
+});
+
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    // Error occurred - check `error.message` for more details.
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    // do something with the locations captured in the background
+    console.log(locations);
   }
 });
