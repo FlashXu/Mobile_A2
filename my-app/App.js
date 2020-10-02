0463a8e265c8335ca0649d01beeec057c269c0e1
seiro <seiroka@outlook.com>
2020-10-02 22:13:02 +0800

    tested version

--- my-app/App.js
+++ my-app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity, StyleSheet, View } from "react-native";
+import { Text, TouchableOpacity, StyleSheet, View, Dimensions } from "react-native";
 import * as TaskManager from "expo-task-manager";
 import * as Location from "expo-location";
 
@@ -7,6 +7,9 @@ const LOCATION_TASK_NAME = "background-location-task";
 
 export default class Component extends React.Component {
   onPress = async () => {
+
+    // console.log("on press works!");
+
     const { status } = await Location.requestPermissionsAsync();
     if (status === "granted") {
       await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
@@ -17,17 +20,24 @@ export default class Component extends React.Component {
 
   render() {
     return (
-      <TouchableOpacity onPress={this.onPress}>
-        <Text>Enable background location</Text>
-      </TouchableOpacity>
+      <View style = {styles.container}>
+        <TouchableOpacity onPress={this.onPress}>
+          <Text>Enable background location</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
+    justifyContent:'center',
+    alignItems:'center',
+    position:'absolute',
+    height:Dimensions.get('window').height,
+    width:Dimensions.get('window').width,
+    zIndex:10,
+  }
 });
 
 TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
@@ -37,6 +47,7 @@ TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
   }
   if (data) {
     const { locations } = data;
+
     // do something with the locations captured in the background
     console.log(locations);
   }
